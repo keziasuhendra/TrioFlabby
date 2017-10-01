5121a00e6d8f63d77360f140d3b3d55e04636690
keziasuhendra <keziasuhendra44@gmail.com>
2017-10-01 22:01:40 +0700

    profile template

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+	require 'connection.php';
+	session_start();
+	if ($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		if (isset($_POST['login']))
+		{
+			require 'loginprocess.php';
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ojek Panas</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<div id="signin-rectangle">
+		<div class="title"><span>LOGIN</span></div>
+		<form method="POST" action="index.php">
+			<table class="form" border="0">
+				<tr>
+					<td><label class="label" for="username">Username</label></td>
+					<td><input class="text-field" type="text" name="username" id="username" maxlength="20" required></td>
+				</tr>	
+				<tr>
+					<td><label class="label" for="pass">Password</label></td>
+					<td><input class="text-field" type="password" name="pass" id="pass" maxlength="16" required></td>
+				</tr>
+			</table>
+			<div class="empty-space"></div>
+			<div class="block-container">
+				<div class="no-account"><a href="signup.php">Don't have an account?</a></div>
+				<button class="button" name="login">GO!</button>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
---
+++ loginprocess.php
@@ -0,0 +1,16 @@
+<?php
+	$username = $mysqli->escape_string($_POST['username']);
+	$result = $mysqli->query("SELECT * FROM user WHERE username='$username'");
+	if ($result->num_rows == 0) {
+		echo "<script>alert('Akun dengan username tersebut belum ada')</script>";
+	} else {
+		$user = $result->fetch_assoc();
+		if (password_verify($_POST['pass'], $user['password'])) {
+			$_SESSION['username'] = $user['username'];
+			$_SESSION['logged in'] = true;
+			header("location: profile.php");
+		} else {
+			echo "<script>alert('Password salah !')</script>";
+		}
+	}
+?>
