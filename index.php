d0e372087525e8a468cf5e568cdc1e2b02b0fe45
Kezia Suhendra <keziasuhendra44@gmail.com>
2017-10-05 10:46:54 +0700

    ajax

--- index.php
+++ index.php
@@ -1,87 +1,7 @@
-<<<<<<< HEAD
-<!-- <html>
-<head>
-<script>
-function validate(field, query) {
-  var xmlhttp;
-
-  if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-  console.log(field);
-  console.log(query);
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState != 4 && xmlhttp.status == 200) {
-      document.getElementById(field).innerHTML = "Validating..";
-    } else if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
-    } else {
-      document.getElementById("txtHint").innerHTML = "Error Occurred. <a href='signup.php'>Reload Or Refresh</a> the page.";
-    }
-  }
-
-  xmlhttp.open("GET", "getUser.php?field=" + field + "&query=" + query, true);
-  xmlhttp.send();
-}
-
-function showUser(str) {
-    if (str == "") {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else { 
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","getuser.php?q="+str,true);
-        xmlhttp.send();
-    }
-}
-</script>
-</head>
-<body>
-<div id="signup-rectangle">
-    <div class="title"><span>SIGN UP</span></div>
-    <form name="register-form" action="signup.php" method="POST" onsubmit="return validateForm1()">
-      <table class="form" border="0">
-        <tr>
-          <td><label class="label" for="fullname">Your Name</label></td>
-          <td colspan="2"><input class="text-field" type="text" name="fullname" id="fullname"></td>
-        </tr>
-        <tr>
-          <td><label class="label" for="username">Username</label></td>
-          <td><input class="small-text-field" type="text" name="username" id="username" maxlength="20" onblur="validate('username', this.value)"></td>
-          <td><div class="check">&#x2716</div></td>
-        </tr>
-        <tr>
-          <td><label class="label" for="email">Email</label></td>
-          <td><input class="small-text-field" type="email" name="email" id="email" maxlength="20" onblur="validate('email', this.value)"></td>
-          <td><div class="check">&#x2716</div></td>
-        </tr>
-      
-      </div>
-    </form>
-
-    <div id="txtHint"><b>Person info will be listed here...</b></div>
-  </div>
-</body>
-</html> -->
-=======
 <?php
 	require 'connection.php';
 	session_start();
-	
+
 	if (isset($_SESSION['id'])){ // redirect if hasn't logged out
 		header("location: profile.php?id_active=$_SESSION[id]");
 	}
@@ -94,7 +14,6 @@ function showUser(str) {
 		}
 	}
 ?>
->>>>>>> 8552b0888e8c605f17f918dfb11d3c36c117b501
 
 <!DOCTYPE html>
 <html>
